8470f5923f990d85c8e54690f8455db628d5128f
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-10-10 16:43:53 -0500

    Adds dominoes prefab

---
+++ Assets/Scripts/MachineFSM.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// This enumeration will be kept fully public so that it can be referenced
+/// globally by other scripts.
+/// </summary>
+public enum rubeState
+{
+    init = 0,
+
+
+}
+
+public class MachineFSM : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MachineManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MachineManager : MonoBehaviour {
+
+    private MachineManager theMachineManager;
+    private MachineFSM theFSM;
+
+    private MachineManager()
+    {
+        
+    }
+
+    public MachineManager getManager()
+    {
+        if(theMachineManager == null)
+            theMachineManager = new MachineManager();
+        return theMachineManager;
+    }
+}
