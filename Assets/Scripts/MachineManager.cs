8ba665274ffef303c5476ce0290045de0e10a637
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-10-20 16:19:14 -0500

    comments

--- Assets/Scripts/MachineManager.cs
+++ Assets/Scripts/MachineManager.cs
@@ -2,12 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// The MachineManager class represents a Singleton tha contains both the
+/// FSM controlling the rube goldberg machine and the subject of the observer
+/// pattern. It sends messages to the subscribed objects in the scene, 
+/// updating them on the current state of the FSM.
+/// </summary>
 public class MachineManager : MonoBehaviour {
-
+    /// <summary>
+    /// The list of MonoObservers that will be updated when the state changes
+    /// </summary>
     private List<MonoObserver> subscribers = new List<MonoObserver>();
+
+    /// <summary>
+    /// Static reference to the FSM controlling the machine.
+    /// </summary>
     private static MachineFSM theFSM;
+
+    /// <summary>
+    /// Static reference to the singleton instance of the MachineManager
+    /// </summary>
     private static MachineManager instance;
+
+    /// <summary>
+    /// Static reference to the singleton GameObject that the monobehavior
+    /// gets attached
+    /// </summary>
     private static GameObject theManager;
+    
+    /// <summary>
+    /// Instance is the accessor property for the singleton object. It 
+    /// implements the lazy instantiation of both the gameobject and the
+    /// MonoBehaviors needed
+    /// </summary>
     public static MachineManager Instance
     {
         get 
@@ -32,12 +59,11 @@ public class MachineManager : MonoBehaviour {
         }
     }
 
-
-
     private void Awake()
     {
         instance = this;
     }
+
 
     public void subscribe(MonoObserver obs)
     {
